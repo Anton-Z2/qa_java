a78be6fd0efa5dbc2dbf2225789507e9e44c7ee6
Zubchevsky A.V. <zubchevsckij-av@yandex.ru>
2024-08-31 13:33:47 +0300

    Убрал бессмысленные проверки при явном вызове методов

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -10,36 +11,33 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
-    @Spy
     private Feline feline;
 
+    @Before
+    public void setUp(){
+        feline = new Feline();
+    }
 
     @Test
     public void testEatMeat() throws Exception{
         assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
-        Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
     }
 
     @Test
     public void testGetFamily() {
         assertEquals("Кошачьи", feline.getFamily());
-        Mockito.verify(feline, Mockito.times(1)).getFamily();
     }
 
     @Test
     public void testGetKittensNoArg() {
         assertEquals(1, feline.getKittens());
-        Mockito.verify(feline, Mockito.times(1)).getKittens();
     }
 
     @Test
     public void testGetKittensWithArg() {
         assertEquals(4, feline.getKittens(4));
-        Mockito.verify(feline).getKittens(Mockito.anyInt());
-        Mockito.verify(feline, Mockito.times(1)).getKittens(4);
     }
 
 }
